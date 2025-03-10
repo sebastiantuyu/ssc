4718ebdff04325e0e20cf00eef55394b9372dab4
Sebastiano Tuyu <alextpineiro@gmail.com>
2025-03-10 07:50:49 -0600

    chore: reduce installation await

--- config-server.sh
+++ config-server.sh
@@ -17,7 +17,11 @@ sudo apt install -y nginx
 sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 
-sleep 60
+sleep 5
 
 sudo snap install --classic certbot
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
+
+echo "========================"
+echo "Done installing."
+echo "========================"
